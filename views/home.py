0b1940f090c99f6d7662391b5d5415ce7d21b39f
Floskinner <58706771+Floskinner@users.noreply.github.com>
2022-01-06 15:54:42 +0100

    added some expample

---
+++ views/home.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+import fastapi
+from starlette.requests import Request
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates("templates")
+router = fastapi.APIRouter()
+
+
+@router.get("/")
+def index(request: Request):
+    """Return the renderd template for the /index.html
+
+    Args:
+        request (Request): Requerd for Template
+    """
+    some_data_day = datetime.today().day
+    return templates.TemplateResponse("home/index.html", {"request": request, "day": some_data_day})
