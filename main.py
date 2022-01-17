d1fbfa84e8e120f28731650607f72811e6935f34
Florian Glaser <58706771+Floskinner@users.noreply.github.com>
2022-01-17 19:53:37 +0100

    Add google request (#1)
    * updated requirements (#2)
    
    * Added Bootstrap (#3)
    
    * Added API Conf
    
    * added pydantic, requests, pytest
    
    * added google api infrastructure and methods
    
    * removed unused import
    
    * added logging
    
    * changed from requests to httpx
    
    * restructured and switched to httpx
    
    * added pytests for gapi
    
    * switched to httpx
    
    * added logging
    
    * added own Google Exception
    
    * added docstrings
    
    * Add google request (#4)
    
    * updated requirements
    
    * Added API Conf
    
    * added pydantic, requests, pytest
    
    * added google api infrastructure and methods
    
    * removed unused import
    
    * added logging
    
    * changed from requests to httpx
    
    * restructured and switched to httpx
    
    * added pytests for gapi
    
    * switched to httpx
    
    * added logging
    
    * added own Google Exception
    
    * added docstrings
    
    Co-authored-by: Baldur Siegel <43816694+baldur132@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+"""Main entry Point for the Application"""
+import logging
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
 import fastapi
 import uvicorn
-
 from starlette.staticfiles import StaticFiles
+
 from views import home
 
 app = fastapi.FastAPI()
@@ -9,17 +14,43 @@ app = fastapi.FastAPI()
 
 def configure():
     """Init Setup for the application"""
+    configure_logger()
     configure_routing()
     configure_database()
 
 
+def configure_logger():
+    """Configure root logger"""
+    logging_path = Path("./logs/infos.log")
+    logger = logging.getLogger()
+
+    # create handler
+    stream_h = logging.StreamHandler()
+    file_h = logging.FileHandler(logging_path)
+    backup_h = RotatingFileHandler(logging_path, maxBytes=10000, backupCount=5)
+
+    # level and formatter
+    stream_h.setLevel(logging.WARNING)
+    file_h.setLevel(logging.ERROR)
+
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+    stream_h.setFormatter(formatter)
+    file_h.setFormatter(formatter)
+
+    # add handler
+    logger.addHandler(stream_h)
+    logger.addHandler(file_h)
+    logger.addHandler(backup_h)
+
+
 def configure_routing():
+    """Add / Configure all router for FastAPI"""
     app.mount("/static", StaticFiles(directory="static"), name="static")
     app.include_router(home.router)
 
 
 def configure_database():
-    pass
+    """Configure connection to the Database"""
 
 
 if __name__ == "__main__":
---
+++ tests/test_gapi.py
@@ -0,0 +1,84 @@
+import json
+from typing import List
+
+import httpx
+import pytest
+from infrastructure import Cuisine, gapi
+from models.restaurant import Restaurant
+from pytest_httpx import HTTPXMock
+from pytest_mock import MockerFixture
+
+
+@pytest.fixture
+def fake_nearby_search() -> dict:
+    with open("tests/example_nearby_search.json", "r", encoding="utf8") as file:
+        return json.load(file)
+
+
+@pytest.fixture
+def fake_nearby_search_restaurants(fake_nearby_search: str) -> List[Restaurant]:
+    return [Restaurant(**value) for value in fake_nearby_search.get("results")]
+
+
+@pytest.fixture
+def fake_place_details() -> dict:
+    with open("tests/example_place_details.json", "r", encoding="utf8") as file:
+        return json.load(file)
+
+
+@pytest.fixture
+def fake_restaurants() -> List[Restaurant]:
+    with open("tests/example_restaurants.json", "r", encoding="utf8") as file:
+        fake_restaurants = json.load(file)
+        return [Restaurant(**value) for value in fake_restaurants]
+
+
+@pytest.mark.parametrize("status_code", [100, 200, 300, 400])
+def test_nearby_search(
+    mocker: MockerFixture,
+    httpx_mock: HTTPXMock,
+    status_code: int,
+    fake_nearby_search: dict,
+    fake_nearby_search_restaurants: List[Restaurant],
+):
+    # Fake Datas
+    params: dict = {
+        "keyword": Cuisine.DOENER.value,
+        "location": "42,42",
+        "opennow": True,
+        "radius": "42",
+        "type": "restaurant",
+        "language": "de",
+    }
+    # Mock httpx responses
+    httpx_mock.add_response(status_code=status_code, json=fake_nearby_search)
+
+    # Mock other functions
+    mocker.patch("infrastructure.get_api_key", return_value="42")
+
+    if status_code != 200:
+        with pytest.raises(httpx.HTTPStatusError):
+            gapi.nearby_search(params)
+    else:
+        restaurants = gapi.nearby_search(params)
+        assert fake_nearby_search_restaurants == restaurants
+
+
+def test_place_details(
+    httpx_mock: HTTPXMock,
+    fake_place_details: dict,
+    fake_restaurants: List[Restaurant],
+    fake_nearby_search_restaurants: List[Restaurant],
+    mocker: MockerFixture,
+):
+
+    # Mock httpx responses
+    for fake_place_detail in fake_place_details:
+        url = f"https://maps.googleapis.com/maps/api/place/details/json?key=42&place_id={fake_place_detail['result']['place_id']}"
+        httpx_mock.add_response(status_code=200, json=fake_place_detail, url=url)
+
+    # Mock other functions
+    mocker.patch("infrastructure.get_api_key", return_value="42")
+
+    restaurants = gapi.place_details(fake_nearby_search_restaurants)
+    assert fake_restaurants == restaurants
