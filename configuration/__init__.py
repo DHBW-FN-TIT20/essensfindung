ee57d99732a1f0bc2383156a892cd3c4bbb7bdb1
Floskinner <58706771+Floskinner@users.noreply.github.com>
2022-01-29 18:18:42 +0100

    Create conf files if not there

--- configuration/__init__.py
+++ configuration/__init__.py
@@ -1,9 +1,10 @@
+import shutil
 from pathlib import Path
 
 PATH_GOOGLE = Path("./configuration/google_api.conf")
 PATH_DB = Path("./configuration/db.conf")
 
 if not PATH_GOOGLE.exists():
-    PATH_GOOGLE = Path("./configuration/example_google_api.conf")
+    shutil.copy("./configuration/example_google_api.conf", str(PATH_GOOGLE))
 if not PATH_DB.exists():
-    PATH_DB = Path("./configuration/example_db.conf")
+    shutil.copy("./configuration/example_db.conf", str(PATH_DB))
