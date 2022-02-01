7875fa8ff5041c24a748ca288f93da8c7cb63db5
baldur <baldur@siegels.us>
2022-02-01 11:50:03 +0100

    added tos and privacy policy modals

--- views/signin.py
+++ views/signin.py
@@ -23,4 +23,21 @@ def register(request: Request):
     Args:
         request (Request): Requerd for Template
     """
-    return templates.TemplateResponse("signin/register.html", {"request": request})
+
+    #read terms of service and privacy policy from text files
+    tosstring = "Terms of Service is Missing"
+    privstring = "Privacy Policy is Missing"
+
+    try:
+        with open("static/text/privacy.txt", "r", encoding="utf-8") as privfile:
+            privstring = "".join(privfile.readlines())
+        with open("static/text/tos.txt", "r", encoding="utf-8") as tosfile:
+            tosstring = "".join(tosfile.readlines())
+
+        print(tosstring)
+    except Exception as e:
+        print(e)
+    
+    legal = {"tos": tosstring, "privacy": privstring}
+        
+    return templates.TemplateResponse("signin/register.html",{"request": request, "legal": legal})
