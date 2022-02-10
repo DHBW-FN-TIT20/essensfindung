c3f0984839f64d6489ebdb5d6191f23cb791c356
Florian Herkommer <florian.herkommer@gmx.de>
2022-02-10 17:26:08 +0100

    defined route

--- app/views/rating.py
+++ app/views/rating.py
@@ -1,8 +1,15 @@
 import fastapi
+from fastapi import Depends
 from fastapi.responses import HTMLResponse
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
+from db.database import get_db
+from sqlalchemy.orm import Session
 
 templates = Jinja2Templates("templates")
 router = fastapi.APIRouter()
 
+@router.get("/rating", response_class=HTMLResponse)
+async def rating(
+    db_session: Session = Depends(get_db)
+):
