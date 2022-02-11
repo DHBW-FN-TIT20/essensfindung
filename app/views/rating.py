9990ed0ad113a9391ceece0d34aac2b7c4034682
Florian Herkommer <florian.herkommer@gmx.de>
2022-02-11 08:44:42 +0100

    added link to ratings to main_navbar

--- app/views/rating.py
+++ app/views/rating.py
@@ -5,11 +5,21 @@ from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 from db.database import get_db
 from sqlalchemy.orm import Session
+from tools.security import get_current_user
+from schemes.scheme_user import User
+from services import service_res
 
 templates = Jinja2Templates("templates")
 router = fastapi.APIRouter()
 
+
 @router.get("/rating", response_class=HTMLResponse)
 async def rating(
-    db_session: Session = Depends(get_db)
+    request: Request,
+    db_session: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
 ):
+    rest_ratings = service_res.get_assessments_from_user(db_session=db_session, user=current_user)
+    return templates.TemplateResponse(
+        "rating/rating.html", {"request": request, "rest_ratings":rest_ratings}
+    )
