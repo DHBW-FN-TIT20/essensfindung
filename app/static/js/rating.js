e2aa9780e0da575ae6b8cd96d6bec129e32b4ea7
baldur <baldur@siegels.us>
2022-02-20 13:31:41 +0100

    Added alphabetic sorting and refactored

--- app/static/js/rating.js
+++ app/static/js/rating.js
@@ -3,7 +3,7 @@
 //contains objects corresponding to each entry on the page
 let entries = []
 
-function sortDOMTime(list, direction, parent) {
+function sortDOMbyTime(list, direction, parent) {
     //bubble sorts a list of objects as well as the DOM according to a UNIX timestamp
     //  list: array of objects (see "generating objects for each page entry")
     //  direction: bool that changes sort direction, true for asc and false for desc
@@ -33,7 +33,7 @@ function sortDOMTime(list, direction, parent) {
         n = n - 1;
     } while (s);
 }
-function sortDOMRating(list, direction, parent) {
+function sortDOMbyRating(list, direction, parent) {
     //bubble sorts a list of objects as well as the DOM according to the object rating
     //  list: array of objects (see "generating objects for each page entry")
     //  direction: bool that changes sort direction, true for asc and false for desc
@@ -63,116 +63,39 @@ function sortDOMRating(list, direction, parent) {
         n = n - 1;
     } while (s);
 }
-
-function init() {
-    //generating objects for each page entry
-    document.querySelectorAll(".entry").forEach(e => {
-        //parse rating
-        rating = e.querySelector(".entry-rating").innerText.trim();
-        rating = (rating === "Keine Bewertung") ? 0 : parseInt(rating.slice(0, 1));
-
-        //create entry object
-        entries.push(
-            {
-                name: e.querySelector(".entry-name").innerText,
-                rating: rating,
-                timestamp: parseInt(e.querySelector(".entry-timestamp").innerText),
-                parent: e.parentNode.parentNode
-            }
-        );
-    });
-
-    /* event listeners */
-
-    //filters entries with a rating
-    document.getElementById("filterWithBewertung").addEventListener("click", e => {
-        if (e.target.checked) {
-            //show entries with a rating
-            for (i = 0; i < entries.length; i++) {
-                if (entries[i].rating !== 0) {
-                    //entry has a rating
-                    entries[i].parent.classList.remove("d-none");
+function sortDOMbyName(list, direction, parent) {
+    //bubble sorts a list of objects as well as the DOM according to the entry name
+    //  list: array of objects (see "generating objects for each page entry")
+    //  direction: bool that changes sort direction, true for asc and false for desc
+    //  parent: node of the parent element of the dom elements to be sorted
+    let s, n = list.length;
+    do {
+        s = false;
+        for (i = 1; i < n; i++) {
+            if (direction) {
+                if (list[i - 1].name.toLowerCase() > list[i].name.toLowerCase()) {
+                    parent.insertBefore(list[i].parent, list[i - 1].parent);
+                    x = list[i];
+                    list[i] = list[i - 1];
+                    list[i - 1] = x;
+                    s = true;
                 }
-            }
-        } else {
-            //hide entries with a rating
-            for (i = 0; i < entries.length; i++) {
-                if (entries[i].rating !== 0) {
-                    //entry has a rating
-                    entries[i].parent.classList.add("d-none");
+            } else {
+                if (list[i - 1].name.toLowerCase() > list[i].name.toLowerCase()) {
+                    parent.insertBefore(list[i].parent, list[i - 1].parent);
+                    x = list[i];
+                    list[i] = list[i - 1];
+                    list[i - 1] = x;
+                    s = true;
                 }
             }
         }
-    });
-    //filters entries without a rating
-    document.getElementById("filterWithoutBewertung").addEventListener("click", e => {
-        if (e.target.checked) {
-            //show entries without a rating
-            for (i = 0; i < entries.length; i++) {
-                if (entries[i].rating === 0) {
-                    //entry does not have a rating
-                    entries[i].parent.classList.remove("d-none");
-                }
-            }
-        } else {
-            //hide entries without a rating
-            for (i = 0; i < entries.length; i++) {
-                if (entries[i].rating === 0) {
-                    //entry does not have a rating
-                    entries[i].parent.classList.add("d-none");
-                }
-            }
-        }
-    });
+        n = n - 1;
+    } while (s);
+}
 
-    //sorting functions
-    //sorts by date descending
-    document.getElementById("sortDateDesc").addEventListener("click", e => {
-        console.log("sorting date desc");
-        if (e.target.checked) {
-            //sort by date desc
-
-            //contains the parent element
-            parent = document.querySelector(".entry").parentNode.parentNode.parentNode;
-            
-            //bubble sort to rearrange dom
-            sortDOMTime(entries, false, parent);
-        }
-    });
-    //sorts by date ascending
-    document.getElementById("sortDateAsc").addEventListener("click", e => {
-        console.log("sorting date asc");
-        if (e.target.checked) {
-            //contains the parent element
-            parent = document.querySelector(".entry").parentNode.parentNode.parentNode;
-            
-            //bubble sort to rearrange dom
-            sortDOMTime(entries, true, parent);
-        }
-    });
-    //sorts by rating descending
-    document.getElementById("sortRatingDesc").addEventListener("click", e => {
-        console.log("sorting rating desc");
-        if (e.target.checked) {
-            //contains the parent element
-            parent = document.querySelector(".entry").parentNode.parentNode.parentNode;
-
-            //bubble sort to rearrange dom
-            sortDOMRating(entries, false, parent);
-        }
-    });
-    //sorts by rating asc
-    document.getElementById("sortRatingAsc").addEventListener("click", e => {
-        console.log("sorting rating asc");
-        if (e.target.checked) {
-            //contains the parent element
-            parent = document.querySelector(".entry").parentNode.parentNode.parentNode;
-
-            //bubble sort to rearrange dom
-            sortDOMRating(entries, true, parent);
-        }
-    });
-
+function initDrawers() {
+    //creates event listeners to enable Filter/Sort Drawer functionality
 
     /* filter and sort drawer functionality */
 
@@ -223,4 +146,128 @@ function init() {
             sortButtons.classList.remove("d-flex");
         }
     });
+}
+
+function initSort() {
+    //creates event listeners for sort buttons
+
+    //sorting functions
+    //sorts by date descending
+    document.getElementById("sortDateDesc").addEventListener("click", e => {
+        console.log("sorting date desc");
+        if (e.target.checked) {
+            //sort by date desc
+
+            //contains the parent element
+            parent = document.querySelector(".entry").parentNode.parentNode.parentNode;
+            
+            //bubble sort to rearrange dom
+            sortDOMbyTime(entries, false, parent);
+        }
+    });
+    //sorts by date ascending
+    document.getElementById("sortDateAsc").addEventListener("click", e => {
+        console.log("sorting date asc");
+        if (e.target.checked) {
+            //contains the parent element
+            parent = document.querySelector(".entry").parentNode.parentNode.parentNode;
+            
+            //bubble sort to rearrange dom
+            sortDOMbyTime(entries, true, parent);
+        }
+    });
+    //sorts by rating descending
+    document.getElementById("sortRatingDesc").addEventListener("click", e => {
+        console.log("sorting rating desc");
+        if (e.target.checked) {
+            //contains the parent element
+            parent = document.querySelector(".entry").parentNode.parentNode.parentNode;
+
+            //bubble sort to rearrange dom
+            sortDOMbyRating(entries, false, parent);
+        }
+    });
+    //sorts by rating asc
+    document.getElementById("sortRatingAsc").addEventListener("click", e => {
+        console.log("sorting rating asc");
+        if (e.target.checked) {
+            //contains the parent element
+            parent = document.querySelector(".entry").parentNode.parentNode.parentNode;
+
+            //bubble sort to rearrange dom
+            sortDOMbyRating(entries, true, parent);
+        }
+    });
+}
+function initFilter() {
+    //creates event listeners for filter buttons
+
+    //filters entries with a rating
+    document.getElementById("filterWithBewertung").addEventListener("click", e => {
+        if (e.target.checked) {
+            //show entries with a rating
+            for (i = 0; i < entries.length; i++) {
+                if (entries[i].rating !== 0) {
+                    //entry has a rating
+                    entries[i].parent.classList.remove("d-none");
+                }
+            }
+        } else {
+            //hide entries with a rating
+            for (i = 0; i < entries.length; i++) {
+                if (entries[i].rating !== 0) {
+                    //entry has a rating
+                    entries[i].parent.classList.add("d-none");
+                }
+            }
+        }
+    });
+    //filters entries without a rating
+    document.getElementById("filterWithoutBewertung").addEventListener("click", e => {
+        if (e.target.checked) {
+            //show entries without a rating
+            for (i = 0; i < entries.length; i++) {
+                if (entries[i].rating === 0) {
+                    //entry does not have a rating
+                    entries[i].parent.classList.remove("d-none");
+                }
+            }
+        } else {
+            //hide entries without a rating
+            for (i = 0; i < entries.length; i++) {
+                if (entries[i].rating === 0) {
+                    //entry does not have a rating
+                    entries[i].parent.classList.add("d-none");
+                }
+            }
+        }
+    });
+}
+
+function init() {
+    //generating objects for each page entry
+    document.querySelectorAll(".entry").forEach(e => {
+        //parse rating
+        rating = e.querySelector(".entry-rating").innerText.trim();
+        rating = (rating === "Keine Bewertung") ? 0 : parseInt(rating.slice(0, 1));
+
+        //create entry object
+        entries.push(
+            {
+                name: e.querySelector(".entry-name").innerText,
+                rating: rating,
+                timestamp: parseInt(e.querySelector(".entry-timestamp").innerText),
+                parent: e.parentNode.parentNode
+            }
+        );
+    });
+
+    //create event listeners for page functionality
+    initDrawers();
+    initSort();
+    initFilter();
+
+    //sort list alphabetically
+    parent = document.querySelector(".entry").parentNode.parentNode.parentNode;
+    sortDOMbyName(entries, true, parent);
 }
