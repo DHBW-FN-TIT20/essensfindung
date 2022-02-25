fe7f82cb39c1325eb544ef7c585b7cc71abc4a94
baldur <baldur@siegels.us>
2022-02-25 13:06:26 +0100

    Redesigned recipe result page

--- app/views/recipe.py
+++ app/views/recipe.py
@@ -36,16 +36,20 @@ async def findrecipe(
     recipe = service_rec.search_recipe(recipe_filter=rec_filter)
 
     prep_time_total_seconds = recipe.prepTime.total_seconds()
-    prep_time_hours = prep_time_total_seconds // 3600
-    prep_time_minutes = (prep_time_total_seconds % 3600) // 60
-    prep_time_seconds = prep_time_total_seconds % 60
-    cook_time_total_seconds = recipe.cookTime.total_seconds()
-    cook_time_hours = cook_time_total_seconds // 3600
-    cook_time_minutes = (cook_time_total_seconds % 3600) // 60
-    cook_time_seconds = cook_time_total_seconds % 60
+    prep_time_days = int(prep_time_total_seconds // 86400)
+    prep_time_hours = int((prep_time_total_seconds % 86400) // 3600)
+    prep_time_minutes = int((prep_time_total_seconds % 3600) // 60)
+    prep_time_seconds = int(prep_time_total_seconds % 60)
 
-    prep_time = f"{int(prep_time_hours)} Stunde(n), {int(prep_time_minutes)} Minute(n), {int(prep_time_seconds)} Sekunde(n)"
-    cook_time = f"{int(cook_time_hours)} Stunde(n), {int(cook_time_minutes)} Minute(n), {int(cook_time_seconds)} Sekunde(n)"
+    cook_time_total_seconds = recipe.cookTime.total_seconds()
+    cook_time_days = int(cook_time_total_seconds // 86400)
+    cook_time_minutes = int((cook_time_total_seconds % 86400) // 3600)
+    cook_time_hours = int(cook_time_total_seconds // 3600)
+    cook_time_minutes = int((cook_time_total_seconds % 3600) // 60)
+    cook_time_seconds = int(cook_time_total_seconds % 60)
+
+    prep_time = {"days": prep_time_days, "hours": prep_time_hours, "minutes": prep_time_minutes, "seconds": prep_time_seconds}
+    cook_time = {"days": cook_time_days, "hours": cook_time_hours, "minutes": cook_time_minutes, "seconds": cook_time_seconds}
 
     return templates.TemplateResponse(
         "recipe/recipe_result.html", {"request": request, "recipe": recipe, "prepTime": prep_time, "cookTime": cook_time}
